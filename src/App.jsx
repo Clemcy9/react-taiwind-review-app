21765907a03b6b3dd9dacd72d80cb485e33f2f19
Sharzy <bekemini@yahoo.com>
2025-06-17 14:58:53 +0100

    update: tried editing app.js, test run

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,9 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import React from 'react'
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-    </>
-  );
+    <div>App</div>
+  )
 }
 
-export default App;
+export default App
