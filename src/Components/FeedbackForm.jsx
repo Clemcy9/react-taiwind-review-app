910167688883a882e022108b5a319dab6e977b2a
kings0001 <donswanta@gmail.com>
2025-06-18 07:34:50 +0100

    edit feedback form

--- src/Components/FeedbackForm.jsx
+++ src/Components/FeedbackForm.jsx
@@ -2,20 +2,19 @@ import React from 'react'
 
 function FeedbackForm() {
   return (
-    <div className='ml-20 mt-20 w-11/12 border border-[#33026C] rounded h-100'> 
+    <div className=' mt-20 m-auto w-11/12 border border-[#33026C] rounded min-h-full mb-8 p-8'> 
         <h3 className='text-center text-4xl font-medium' >General Feedback</h3>
-        <p className='text-center text-xl'>Let us know how your overall experience have been</p>
+        <p className='text-center'>Let us know how your overall experience have been</p>
 
-        <form action="" className='flex flex-col w-full justify-center gap-x-10'>
-            <label htmlFor="">Name</label> 
-            <input type="text " className='w-4/5' placeholder='Jane Doe' />
+        <form action="#" className=' m-auto min-w-full px-8 ' >
+            <label htmlFor="fullName" className='block '>Name</label> 
+            <input type="text " placeholder='Jane Doe' id='fullName' className='min-w-full min-h-10 block m-auto rounded border border-black mb-8 pl-2' />
 
-            <label htmlFor="">Email</label>
-            <input type="email" placeholder='janedoe@gmail.com' />
+            <label htmlFor="email" className='block'>Email</label>
+            <input type="email" placeholder='janedoe@gmail.com' id='email'className='min-w-full min-h-10 block m-auto border border-black mb-8 pl-2' />
             
-            <label htmlFor="">Your Feedback</label>
-            <input type="textarea" placeholder='Type here' />
-            
+            <label htmlFor="feedback" className='block'>Your Feedback</label>
+            <textarea name="feedback" id="feedback" placeholder='Type here' className='min-w-full min-h-30 block m-auto border border-black pl-2'></textarea>
         </form>
     </div>
   )
