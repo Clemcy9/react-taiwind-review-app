383b90fcb78416259f66f3856edfc4f873f4ec7d
kings0001 <donswanta@gmail.com>
2025-06-18 09:18:43 +0100

    update on the feedbackpage

--- src/Components/Article.jsx
+++ src/Components/Article.jsx
@@ -9,7 +9,7 @@ function Article() {
          <p className='mt-6 max-w-lg'>Tell us how satisified you are with our services and how can we serve you better</p>
          </div>
        
-       <div className='mr-20 max-w-md mt-10 md:mt-0'>
+       <div className='mr-20 max-w-md mt-10 md:mt-0 flex-1'>
         <img src={InfoGraphic} alt="info graohics"/>
        </div>
        
--- src/Pages/Feedbackpage.jsx
+++ src/Pages/Feedbackpage.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
-import Teacher from "../assets/teacher.jpg"
+import Teacher from "../assets/baner-pic.png"
 
 function Feedbackpage() {
   return (
-    <div className='flex bg-[#3D0073] text-white justify-around' >
-        <div  >
-            <h3 className="text-2xl font-medium text-white-700 mb-2">Innovate. Connect. Transform</h3>
-            <p className="text-base text-white-700 leading-relaxed mb-6">Your Tech Future with Netisens ICT</p>
-            <button className="bg-green-500 hover:bg-green-400 rounded px-2">Visit our website to learn more</button>
+    <div className='flex bg-[#33026C] text-white justify-between items-start py-8' >
+        <div className='leading-relaxed max-w-md ml-20' >
+            <h3 className="text-3xl font-medium text-white-700 mb-2">Innovate. Connect. Transform</h3>
+            <p className="text-2xl text-white-700 mb-6">Your Tech Future with Netisens ICT</p>
+            <button className="bg-[#1B9407] h-10 text-white hover:bg-green-400 rounded px-2">Visit our website to learn more</button>
         </div>
-        <img src={Teacher} alt="nn" className='rounded w-40 h-32' rounded-md  />
+        <img src={Teacher} alt="netizens class tutor" className='rounded mr-20' rounded-md  />
     </div>
   )
 }
